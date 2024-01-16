ca618ec982b540495c28973a3ed1032db3536d10
okankpl <okan149@gmail.com>
2024-01-16 13:23:09 +0100

    added JSON

---
+++ script.js
@@ -0,0 +1,42 @@
+let questions = [
+    {
+        "question" : "Wer hat HTML erfunden?",
+        "answer_1" : "Robbie Williams",
+        "answer_2" : "Lady Gaga",
+        "answer_3" : "Tim Berners-Lee",
+        "answer_4" : "Justin Bieber",
+        "right_answer" : 3
+    },
+    {
+        "question" : "Welches Tag wird verwendet, um einen Absatz in HTML zu erstellen?",
+        "answer_1" : "<p>",
+        "answer_2" : "<paragraph>",
+        "answer_3" : "<par>",
+        "answer_4" : "<text>",
+        "right_answer" : 1
+    },
+    {
+        "question" : "Welches HTML-Element wird verwendet, um eine Liste mit nummerierten Elementen zu erstellen?",
+        "answer_1" : "<ul>",
+        "answer_2" : "<li>",
+        "answer_3" : "<ol>",
+        "answer_4" : "<dl>",
+        "right_answer" : 3
+    },
+    {
+        "question" : "Welches Attribut wird verwendet, um einen Hyperlink in HTML zu setzen?",
+        "answer_1" : "link",
+        "answer_2" : "href",
+        "answer_3" : "src",
+        "answer_4" : "target",
+        "right_answer" : 2 
+    },
+    {
+        "question" : "Welches Tag wird verwendet, um eine Tabelle in HTML zu erstellen?",
+        "answer_1" : "<table>",
+        "answer_2" : "<tab>",
+        "answer_3" : "<tr>",
+        "answer_4" : "<tb>",
+        "right_answer" : 1
+    }
+];
