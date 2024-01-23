1ec9e8f618a302c9fa99403b1dff0a08882bab82
okankpl <okan149@gmail.com>
2024-01-23 18:09:48 +0100

    added answer func

--- script.js
+++ script.js
@@ -51,8 +51,20 @@ let currentQuestion = 0;
 function showQuestion() {
     let question = questions[currentQuestion];
     document.getElementById("currentQuestion").innerHTML = question["question"];
-    document.getElementById("answer1").innerHTML = question["answer_1"];
-    document.getElementById("answer2").innerHTML = question["answer_2"];
-    document.getElementById("answer3").innerHTML = question["answer_3"];
-    document.getElementById("answer4").innerHTML = question["answer_4"];
+    document.getElementById("answer_1").innerHTML = question["answer_1"];
+    document.getElementById("answer_2").innerHTML = question["answer_2"];
+    document.getElementById("answer_3").innerHTML = question["answer_3"];
+    document.getElementById("answer_4").innerHTML = question["answer_4"];
+}
+
+function answer(selection) {
+    let question = questions[currentQuestion];
+    let selectedQuestionNumber = selection.slice(-1);
+
+    if(selectedQuestionNumber == question['right_answer']) {
+        document.getElementById(selection).parentNode.classList.add('bg-success'); // mit .ParentNode wird auf das übergeordnete Element zugegriffen
+    }
+    else {
+        document.getElementById(selection).parentNode.classList.add('bg-danger');
+    }
 }
